ac3e13e7cdaaa66d14eb5a604aae029690485694
Alex Lee <alexlee8937@gmail.com>
2025-06-16 19:05:02 -0700

    feat: added contact section with contact social icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,17 @@
 const Contact = () => {
-  const footerStyle = 
-  {
-    height: '250px',
-    backgroundColor: 'black'
-  };
   return (
-    <div style={footerStyle} className="contact-sec" id="contact"></div>
+    <div className="contact-sec" id="contact">
+      <div className="contact-sec-left text-white/70 hover:text-white transition">Developed by @Alex Lee</div> 
+      <div className="contact-sec-right">
+        <ul>
+          <li className="social-icons"><a href="https://github.com/alexwoolee" target="_blank"><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/github.svg" alt="github icon" /></a></li>
+          <li className="social-icons"><a href="https://github.com/alexwoolee" target="_blank"><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/twitter-x.svg" alt="github icon" /></a></li>
+          <li className="social-icons"><a href="https://github.com/alexwoolee" target="_blank"><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/linkedin.svg" alt="github icon" /></a></li>
+          <li className="social-icons"><a href="https://github.com/alexwoolee" target="_blank"><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/discord.svg" alt="github icon" /></a></li>
+          <li className="social-icons"><a href="https://github.com/alexwoolee" target="_blank"><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/instagram.svg" alt="github icon" /></a></li>
+        </ul>
+      </div>
+    </div>
   );
 }  
 
