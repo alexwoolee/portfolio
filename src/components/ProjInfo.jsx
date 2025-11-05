2d95b560c61bfbdb9515e32acca5a97c72e2fb44
Alex Lee <alexlee8937@gmail.com>
2025-11-04 22:04:33 -0800

    projects page: fix scroll issue with project info

--- src/components/ProjInfo.jsx
+++ src/components/ProjInfo.jsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import FIGMA from '../assets/icons/figma-logo.svg';
+import React from "react";
+import ProjInfoLinks from "./ProjInfoLinks";
+import "../styles/projects.css";
 
-const ProjInfo = ({project}) => {
+const ProjInfo = ({ project }) => {
   return (
-    <div id="proj-info-display">
-      <p id="proj-authors">{project.authors}</p>
-      <img src={project.img} alt="project info image" className="proj-info-img"></img>
-      <h1 id="proj-lg-title">{project.title}</h1>
-      <p>{project.txt}</p>
-      <div id="github-links">
-        {/* github repo */}
-        <div className="github-link">
-          <a href={project.github}><img src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/github.svg" alt="github icon" /></a>
+    <>
+      <div className="proj-info-container">
+        <div className="proj-column-content">
+          <p id="proj-authors">{project.authors}</p>
+          <img
+            src={project.img}
+            alt="project info image"
+            className="proj-info-img"
+          ></img>
+          <h1 id="proj-lg-title">{project.title}</h1>
+          <p className="proj-info-txt">{project.txt}</p>
         </div>
-        {/* figma */}
-        <div className="github-link">
-          <a href={project.figma}><img src={FIGMA} /></a>
-        </div>
-        {/* live demo */}
-        <div className="github-link">
-          <a href={project.live}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><g fill="none" fillRule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8"/></g></svg></a>
-        </div>
+        <ProjInfoLinks project={project} />
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default ProjInfo
+export default ProjInfo;
