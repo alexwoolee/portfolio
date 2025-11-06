eef8679e0a30f843e725ead79323d2b675e8ac88
Alex Lee <alexlee8937@gmail.com>
2025-11-05 21:22:38 -0800

    react camel case: svg properties to camel case

--- src/components/icons/HomeIcon.jsx
+++ src/components/icons/HomeIcon.jsx
@@ -8,9 +8,9 @@ const HomeIcon = ({ alt, className }) => {
         viewBox="0 0 24 24"
         fill="none"
         stroke="#000000"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={className}
         alt={alt}
       >
