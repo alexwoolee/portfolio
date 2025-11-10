1c75d4bd8a6f3bc96ddbbc56262c3c0e347e312c
Alex Lee <alexlee8937@gmail.com>
2025-11-10 11:48:16 -0800

    page: minor improvement to about page

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -2,23 +2,19 @@ import React from "react";
 import bojji from "../assets/about/ousama-ranking-bojji.gif";
 import "../styles/about.css";
 import AboutSkills from "../components/AboutSkills";
-import Figma from "../components/icons/tools/Figma"
-import Docker from "../components/icons/tools/Docker"
-import Vercel from "../components/icons/tools/Vercel"
-import Jest from "../components/icons/tools/Jest"
-import AWS from "../components/icons/tools/Aws"
-import GitHub from "../components/icons/tools/GitHub"
-import CMake from "../components/icons/tools/CMake"
-
-
-/*
-Section 1: Picture, Name, Title, Contact links
-Resume Link
-Section 2: Short biography
-Section 3: Code tools 
-Section 4: Best Projects
-Section 5: Long biography
-*/
+import AboutGeneralSkills from "../components/AboutGeneralSkills";
+import ExperienceCard from "../components/ExperienceCard";
+import Figma from "../components/icons/tools/Figma";
+import Docker from "../components/icons/tools/Docker";
+import Vercel from "../components/icons/tools/Vercel";
+import Jest from "../components/icons/tools/Jest";
+import AWS from "../components/icons/tools/Aws";
+import GitHub from "../components/icons/tools/GitHub";
+import CMake from "../components/icons/tools/CMake";
+import HMART from "../assets/experience/HMART.jpeg";
+import MOCC from "../assets/experience/MOCC.png";
+import SFU from "../assets/experience/SFU.png";
+import "../styles/exp.css";
 
 const NewAbout = () => {
   return (
@@ -34,65 +30,17 @@ const NewAbout = () => {
           <h2 id="profession" className="text-center">
             Aspiring <strong>Software Developer</strong>
           </h2>
-          {/* <ul className="socials">
-            <li className="social-icon">
-              <a href="https://github.com/alexwoolee" target="_blank">
-                <img
-                  src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/github.svg"
-                  alt="github icon"
-                />
-              </a>
-            </li>
-            <li className="social-icon">
-              <a href="https://x.com/wooalexlee" target="_blank">
-                <img
-                  src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/twitter-x.svg"
-                  alt="X icon"
-                />
-              </a>
-            </li>
-            <li className="social-icon">
-              <a
-                href="https://www.linkedin.com/in/alex-lee-868008287/"
-                target="_blank"
-              >
-                <img
-                  src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/linkedin.svg"
-                  alt="linkedin icon"
-                />
-              </a>
-            </li>
-            <li className="social-icon">
-              <a
-                href="http://discordapp.com/users/423544690646712330"
-                target="_blank"
-              >
-                <img
-                  src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/discord.svg"
-                  alt="discord icon"
-                />
-              </a>
-            </li>
-            <li className="social-icon">
-              <a href="https://www.instagram.com/wooalexlee/" target="_blank">
-                <img
-                  src="https://raw.githubusercontent.com/CLorant/readme-social-icons/main/large/light/instagram.svg"
-                  alt="instagram icon"
-                />
-              </a>
-            </li>
-          </ul> */}
         </div>
         <div className="about-bio-container">
-        <p id="about-bio" className="text-center">
-          with a passion about turning ideas into
-          engaging digital experiences. Currently studying Computing Science at
-          Simon Fraser University, exploring software development,
-          problem-solving, and new technologies. Enjoys building intuitive
-          interfaces, experimenting with creative projects, and finding smart
-          solutions to real-world challenges.
-        </p>
-      </div>
+          <p id="about-bio" className="text-center">
+            with a passion about turning ideas into engaging digital
+            experiences. Currently studying Computing Science at Simon Fraser
+            University, exploring software development, problem-solving, and new
+            technologies. Enjoys building intuitive interfaces, experimenting
+            with creative projects, and finding smart solutions to real-world
+            challenges.
+          </p>
+        </div>
       </div>
       <div className="logo-sliding">
         <div className="logos">
@@ -118,9 +66,37 @@ const NewAbout = () => {
       <h1 className="about-section-title">Programming Languages</h1>
       <AboutSkills></AboutSkills>
       <h1 className="about-section-title">Skills</h1>
-      <AboutSkills></AboutSkills>
+      <AboutGeneralSkills></AboutGeneralSkills>
       <h1 className="about-section-title">Experience</h1>
-      <AboutSkills></AboutSkills>
+      <ExperienceCard
+        img={HMART}
+        company="H-Mart"
+        title="Grocery Clerk"
+        period="Sep. 2023 - Aug. 2024"
+        txt="text"
+      ></ExperienceCard>
+      <ExperienceCard
+        img={MOCC}
+        company="Marpole Oakridge Community Centre"
+        title="Food Hub"
+        period="Sep. 2022 - Dec. 2022"
+        txt="text"
+      ></ExperienceCard>{" "}
+      <h1 className="exp-type">Events</h1>
+      <ExperienceCard
+        img={SFU}
+        company="SFU"
+        title="Fall Hacks 2025"
+        period="Sep. 27th 2025"
+        txt="text"
+      ></ExperienceCard>
+      <ExperienceCard
+        img={SFU}
+        company="SFU"
+        title="Surge 2025"
+        period="Sep. 27th 2025"
+        txt="text"
+      ></ExperienceCard>
     </div>
   );
 };
