2cb9b71eb180fe79128fa2bb8a3350102c318859
Alex Lee <alexlee8937@gmail.com>
2025-12-17 22:31:19 -0800

    cleanup: remove unused files

--- src/routes/record.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-// This will help us connect to the database
-import db from "../db/connection.js";
-
-// This help convert the id from string to ObjectId for the _id
-import { ObjectId } from "mongodb";
-
-const router = express.Router();
