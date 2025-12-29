abe84f74a773561b372fa1de40be3d462bedc222
alexwoolee <alexlee8937@gmail.com>
2025-12-29 03:18:16 -0800

    fix: module css convert

--- src/components/AboutSection/ScrollGallery.jsx
+++ src/components/AboutSection/ScrollGallery.jsx
@@ -5,6 +5,8 @@ import { ChevronLeft, ChevronRight } from 'lucide-react';
 import Osaka from "../../assets/gallery/osaka-night.png";
 import Gold from "../../assets/gallery/gold-gold-gold.png";
 import Bird from "../../assets/gallery/bird-pond.png";
+/* Styles */
+import styles from './about.module.css';
 
 const ScrollGallery = () => {
   const galleryRef = useRef(null);
@@ -23,29 +25,37 @@ const ScrollGallery = () => {
     if (galleryRef.current) {
       galleryRef.current.scrollBy({
         top: 0,
-        left: 260   ,
+        left: 260,
         behavior: "smooth",
       });
     }
   };
 
   return (
-    <div className="gallery-wrapper">
-      <div className="chevron" onClick={scrollLeft}>
+    <div className={styles.galleryWrapper}>
+      <div 
+        className={`${styles.chevron} ${styles.chevronLeft}`} 
+        onClick={scrollLeft}
+      >
         <ChevronLeft color="white" />
       </div>
-      <div className="gallery-ctn" ref={galleryRef}>
-        <img src={Osaka} alt="photo" className="gallery-pic" />
-        <img src={Gold} alt="photo" className="gallery-pic" />
-        <img src={Bird} alt="photo" className="gallery-pic" />
-        <img src={Osaka} alt="photo" className="gallery-pic" />
-        <img src={Gold} alt="photo" className="gallery-pic" />
-        <img src={Bird} alt="photo" className="gallery-pic" />
-        <img src={Osaka} alt="photo" className="gallery-pic" />
-        <img src={Gold} alt="photo" className="gallery-pic" />
-        <img src={Bird} alt="photo" className="gallery-pic" />
+
+      <div className={styles.galleryCtn} ref={galleryRef}>
+        <img src={Osaka} alt="photo" className={styles.galleryPic} />
+        <img src={Gold} alt="photo" className={styles.galleryPic} />
+        <img src={Bird} alt="photo" className={styles.galleryPic} />
+        <img src={Osaka} alt="photo" className={styles.galleryPic} />
+        <img src={Gold} alt="photo" className={styles.galleryPic} />
+        <img src={Bird} alt="photo" className={styles.galleryPic} />
+        <img src={Osaka} alt="photo" className={styles.galleryPic} />
+        <img src={Gold} alt="photo" className={styles.galleryPic} />
+        <img src={Bird} alt="photo" className={styles.galleryPic} />
       </div>
-      <div className="chevron" onClick={scrollRight}>
+
+      <div 
+        className={`${styles.chevron} ${styles.chevronRight}`} 
+        onClick={scrollRight}
+      >
         <ChevronRight color="white" />
       </div>
     </div>
