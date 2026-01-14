fac28de66a7486e2b5772e91a8026e28176f91c0
alexwoolee <alexlee8937@gmail.com>
2026-01-14 14:27:23 -0800

    fix: add nodePolyfills plugin so gray-matter can access buffer

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,23 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    // Makes buffer available to gray-matter library
+    nodePolyfills({
+      include: ["buffer", "process", "util"],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
   assetsInclude: ["**/*.glb"],
   optimizeDeps: {
     esbuildOptions: {
